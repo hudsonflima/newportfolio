1ecc0485f0501e1934f79e81588109ccb8c36968
Hudson Lima <hudsonflima@gmail.com>
2024-06-20 18:33:06 -0300

    ajuste path download

--- app/page.jsx
+++ app/page.jsx
@@ -34,7 +34,7 @@ const Home = () => {
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
-                href="/path/para/CV_Hudson_Lima.pdf"  // Substitua pelo caminho correto do seu arquivo PDF
+                href="/assets/CV_Hudson_Lima.pdf"  // Substitua pelo caminho correto do seu arquivo PDF
                 download="CV_Hudson_Lima.pdf"        // Nome do arquivo para download
                 onClick={handleDownload}
                 variant="outline"
